c03b2f6cd1a434e1113ef57bbdce53de6d21251c
Jeeyoung Kim <jeeyoungk@gmail.com>
2012-03-16 15:16:17 -0400

    Starting to add d3 unit tests. the d3_array_map / forEach implementation doesn't use underscore.js

--- d3.v2.js
+++ d3.v2.js
@@ -41,13 +41,43 @@ if(_setAttributeNS) {
   }
 }
 var d3_array_map;
-if (Array.prototype.map) {
-  d3_array_map = function(array, each) {
-    return array.map(each);
+var d3_array_forEach;
+;(function() {
+"use strict";
+// Implementation copied from underscore.js
+var ArrayProto         = Array.prototype
+var nativeForEach      = ArrayProto.forEach
+var nativeMap          = ArrayProto.map
+var breaker = {};
+
+d3_array_map = function(obj, iterator, context) {
+  var results = [];
+  if (obj == null) return results;
+  if (nativeMap && obj.map === nativeMap) return obj.map(iterator, context);
+  d3_array_forEach(obj, function(value, index, list) {
+    results[results.length] = iterator.call(context, value, index, list);
+  });
+  if (obj.length === +obj.length) results.length = obj.length;
+  return results;
+};
+
+d3_array_forEach = function(obj, iterator, context) {
+  if (obj == null) return;
+  if (nativeForEach && obj.forEach === nativeForEach) {
+    obj.forEach(iterator, context);
+  } else if (obj.length === +obj.length) {
+    for (var i = 0, l = obj.length; i < l; i++) {
+      if (i in obj && iterator.call(context, obj[i], i, obj) === breaker) return;
+    }
+  } else {
+    for (var key in obj) {
+      if (_.has(obj, key)) {
+        if (iterator.call(context, obj[key], key, obj) === breaker) return;
+      }
+    }
   }
-} else {
-  d3_array_map = _.map;
-}
+};
+})();
 var d3_selectionPrototype_text;
 var hasTextContent;
 
@@ -9165,7 +9195,7 @@ d3.time.dayOfYear = function(date) {
   var year = d3.time.year(date);
   return Math.floor((date - year) / 864e5 - (date.getTimezoneOffset() - year.getTimezoneOffset()) / 1440);
 };
-d3_time_weekdays.forEach(function(day, i) {
+d3_array_forEach(d3_time_weekdays, function(day, i) {
   day = day.toLowerCase();
   i = 7 - i;
 
--- src/compat/map.js
+++ src/compat/map.js
@@ -1,8 +1,38 @@
 var d3_array_map;
-if (Array.prototype.map) {
-  d3_array_map = function(array, each) {
-    return array.map(each);
+var d3_array_forEach;
+;(function() {
+"use strict";
+// Implementation copied from underscore.js
+var ArrayProto         = Array.prototype
+var nativeForEach      = ArrayProto.forEach
+var nativeMap          = ArrayProto.map
+var breaker = {};
+
+d3_array_map = function(obj, iterator, context) {
+  var results = [];
+  if (obj == null) return results;
+  if (nativeMap && obj.map === nativeMap) return obj.map(iterator, context);
+  d3_array_forEach(obj, function(value, index, list) {
+    results[results.length] = iterator.call(context, value, index, list);
+  });
+  if (obj.length === +obj.length) results.length = obj.length;
+  return results;
+};
+
+d3_array_forEach = function(obj, iterator, context) {
+  if (obj == null) return;
+  if (nativeForEach && obj.forEach === nativeForEach) {
+    obj.forEach(iterator, context);
+  } else if (obj.length === +obj.length) {
+    for (var i = 0, l = obj.length; i < l; i++) {
+      if (i in obj && iterator.call(context, obj[i], i, obj) === breaker) return;
+    }
+  } else {
+    for (var key in obj) {
+      if (_.has(obj, key)) {
+        if (iterator.call(context, obj[key], key, obj) === breaker) return;
+      }
+    }
   }
-} else {
-  d3_array_map = _.map;
-}
+};
+})();
