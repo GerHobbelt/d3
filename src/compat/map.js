26622f5edecee3c621b26055c460cb1651fbf0bf
Jeeyoung Kim <jeeyoungk@gmail.com>
2012-04-11 19:05:50 -0400

    override array.prototype.map if it doesn't exist.

--- src/compat/map.js
+++ src/compat/map.js
@@ -35,4 +35,11 @@ d3_array_forEach = function(obj, iterator, context) {
     }
   }
 };
+
+if (!nativeMap) {
+  Array.prototype.map = function(iterator) {
+    return d3_array_map(this, iterator)
+  }
+
+}
 })();
