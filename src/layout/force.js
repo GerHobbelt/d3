f3ac0da7af8cca1fb3951fffde1db27fa55c3d2e
Ger Hobbelt <ger@hobbelt.com>
2014-06-01 20:47:48 +0200

    take the latest force layout code from the 'all_scales_have_subticks' branch:
    - almost all parameters can be functions now
    - augmented Barnes-Hut force calculation
    - quadtree is exported for those who need it
    - fewer divisions in the per-tick calculations: replaced by multiplications, which are probably a tad faster.
    - augmented the Gauss-Seidel approximation to produce less 'craziness' (as in nodes jumping around) for very crowded or otherwise complicated graphs where linked nodes can be very close together or even on top of another without wanting to be so.
    - gravity can now be a function: when it is, you can apply any gravity you want to each of the graph nodes as the gravity function is made responsible for the entire gravity calculation on each tick
    - next to the charges (which can be determined through a user-defined function, in which case they are also updated on every tick) there's the repulsor: a function which can adjust the nodes' positions after the gravity and charges have been applied -- this is the last operation before the verlet integration is performed on every tick
    - `neighbors()` API method which delivers the (linked) neighbors for each node in an array

--- src/layout/force.js
+++ src/layout/force.js
@@ -14,6 +14,19 @@ d3.layout.force = function() {
       size = [1, 1],
       drag,
       alpha,
+      interval,
+      nodes = [],
+      links = [],
+      distances,
+      strengths,
+      neighbors,
+      epsilon = 0.1, // minimal distance-squared for which the approximation holds; any smaller distance is assumed to be this large to prevent instable approximations
+      charges,
+      charge_abssum = -1, // negative value signals the need to recalculate this one
+      gravity_f,
+      theta2_f,
+      has_theta2_f = false,
+      // These model parameters can be either a function or a direct numeric value:
       friction = .9,
       linkDistance = d3_layout_forceLinkDistance,
       linkStrength = d3_layout_forceLinkStrength,
@@ -21,37 +34,76 @@ d3.layout.force = function() {
       chargeDistance2 = d3_layout_forceChargeDistance2,
       gravity = .1,
       theta2 = .64,
-      nodes = [],
-      links = [],
-      distances,
-      strengths,
-      charges;
+      repulsor = false;
 
-  function repulse(node) {
-    return function(quad, x1, _, x2) {
+  setup_model_parameter_functors();
+
+  function setup_model_parameter_functors() {
+    theta2_f = d3_functor(theta2);
+    has_theta2_f = (typeof theta2 === "function");
+  }
+
+  function repulse(node, i) {
+    return function(quad, x1, y1, x2, y2) {
       if (quad.point !== node) {
         var dx = quad.cx - node.x,
             dy = quad.cy - node.y,
             dw = x2 - x1,
-            dn = dx * dx + dy * dy;
+            l = dx * dx + dy * dy,
+            dn = 1 / Math.max(epsilon, l),
+            k = quad.charge * dn,
+            th2;
 
-        /* Barnes-Hut criterion. */
-        if (dw * dw / theta2 < dn) {
-          if (dn < chargeDistance2) {
-            var k = quad.charge / dn;
+        if (has_theta2_f) {
+          // when this is a FUNCTION it calculates theta, NOT theta²!
+          th2 = theta2_f.call(this, node, i, quad, l, x1, x2, k);
+          th2 *= th2;
+        } else {
+          th2 = theta2;
+        }
+
+        /*
+        Based on the Barnes-Hut criterion.
+        http://www.amara.com/papers/nbody.html#tcu
+
+        Uses ideas from A Hierarchical O(N) Force Calculation Algorithm, Dehnen, 2007:
+        http://physics.ucsd.edu/students/courses/winter2008/physics141/lecture16/2002JCP...179...27D.pdf
+        to produce an improved acceptance criterion: Similar to the paper we not only
+        collect the center of mass per quad but also the maximum distance of any
+        node from that derived center of mass by collecting the bounding box;
+        by comparing this measure with the distance of the quad's center of mass to our
+        node and apply a decision threshold based on a (possibly) mass-dependent theta,
+        we can tweak the accuracy of our approximation more accurately for fields with
+        a very uneven charge distribution.
+
+        The basic Barnes-Hut criterion is purely distance based, while we use a criterion
+        which is similar to the potential-based criterion discussed in the latter paper:
+        when the influence of the collective force over the given distance is relatively
+        small, we do accept the consolidated quad data; otherwise we need process the
+        child nodes.
+        */
+        if (l * k * k < th2) {
+          if (l < chargeDistance2) {
+            k *= alpha;
             node.px -= dx * k;
             node.py -= dy * k;
           }
           return true;
         }
 
-        if (quad.point && dn && dn < chargeDistance2) {
-          var k = quad.pointCharge / dn;
+        if (quad.point && isFinite(dn)) {
+          k = quad.pointCharge * alpha * dn;
           node.px -= dx * k;
           node.py -= dy * k;
         }
       }
-      return !quad.charge;
+      return false;
+      //return !quad.charge; -- very dangerous criterion to stop tree traversal as
+      //                        accumulated force may be zero, but close-by so the
+      //                        accumulate must not be used.
+      //                        Another issue is when the node itself coincides
+      //                        with others: those won't be visited if the sum of
+      //                        the charges, including 'node' itself, produce a zero sum.
     };
   }
 
@@ -65,6 +117,7 @@ d3.layout.force = function() {
     var n = nodes.length,
         m = links.length,
         q,
+        f,
         i, // current index
         o, // current object
         s, // current source
@@ -74,58 +127,128 @@ d3.layout.force = function() {
         x, // x-distance
         y; // y-distance
 
-    // gauss-seidel relaxation for links
+    // Gauss-Seidel relaxation for links
     for (i = 0; i < m; ++i) {
       o = links[i];
       s = o.source;
       t = o.target;
       x = t.x - s.x;
       y = t.y - s.y;
-      if (l = (x * x + y * y)) {
-        l = alpha * strengths[i] * ((l = Math.sqrt(l)) - distances[i]) / l;
-        x *= l;
-        y *= l;
-        t.x -= x * (k = s.weight / (t.weight + s.weight));
-        t.y -= y * k;
-        s.x += x * (k = 1 - k);
-        s.y += y * k;
-      }
+      l = x * x + y * y;
+      // We are going to assume that *any* link has a distance and that source and target node
+      // being at the exact same location is a (temporary) mistake. This fixes obscure rendering
+      // artifacts for some graphs.
+      //
+      // At the same time we recognize that users MAY want multiple (linked) nodes to exist at
+      // the *exact same position*: 
+      // for those uses the `linkDistance` can be specified as a *function* which 
+      // SHOULD return *zero*(0) for such links which bind two linked-and-at-the-same-position 
+      // nodes together. By having a distance goal of zero the
+      // relaxation code will help you by attempting to keep the nodes at the same location.
+      // Do note that ensuring both such linked nodes having a mutual charge of *zero* will
+      // help you in achieving the goal of keeping them together like that.
+      //
+      // Meanwhile we dampen the craziness that otherwise ensues for very short distances:  
+      l = Math.max(epsilon, l);
+      // and apply *damped* Gauss-Seidel relaxation:
+      k = Math.sqrt(l);
+      l = alpha * strengths[i] * (k - distances[i]) / l;
+      x *= l;
+      y *= l;
+      // and distribute change based on arity
+      k = s.weight / (t.weight + s.weight);
+      t.x -= x * k;
+      t.y -= y * k;
+      k = 1 - k;
+      s.x += x * k;
+      s.y += y * k;
     }
 
     // apply gravity forces
-    if (k = alpha * gravity) {
-      x = size[0] / 2;
-      y = size[1] / 2;
-      i = -1; if (k) while (++i < n) {
-        o = nodes[i];
-        o.x += (x - o.x) * k;
-        o.y += (y - o.y) * k;
+    //
+    // Note that gravity MAY vary for each node when it is specified as a function:
+    // this may sound rather 'SF' but there are benefits to have when nodes are
+    // more or less 'subject to gravity': special force rendering can be accomplished
+    // without having to go to abject lengths in adding 'hidden nodes' or other hacks
+    // to tweak the charges and thus the forces.
+    // 
+    // Here we let the gravity function adjust the nodes' positions.  
+    if (typeof gravity === "function") {
+      if (alpha) {
+        gravity_f.call(this, nodes, n, alpha, size);
       }
-    }
-
-    // compute quadtree center of mass and apply charge forces
-    if (charge) {
-      d3_layout_forceAccumulate(q = d3.geom.quadtree(nodes), alpha, charges);
-      i = -1; while (++i < n) {
-        if (!(o = nodes[i]).fixed) {
-          q.visit(repulse(o));
+    } else {
+      k = alpha * gravity;
+      if (k) {
+        x = size[0] / 2;
+        y = size[1] / 2;
+        for (i = 0; i < n; ++i) {
+          o = nodes[i];
+          o.x += (x - o.x) * k;
+          o.y += (y - o.y) * k;
         }
       }
     }
 
-    // position verlet integration
-    i = -1; while (++i < n) {
-      o = nodes[i];
-      if (o.fixed) {
-        o.x = o.px;
-        o.y = o.py;
+    // compute quadtree center of mass and apply charge forces
+    q = d3.geom.quadtree(nodes);
+    // recalculate charges on every tick if need be:
+    if (charge_abssum < 0 || typeof charge === "function" || isFinite(chargeDistance2)) {
+      charges = new Array(n);
+      if (typeof charge === "function") {
+        f = 0;
+        for (i = 0; i < n; ++i) {
+          charges[i] = k = +charge.call(this, nodes[i], i, q);
+          f += Math.abs(k);
+        }
       } else {
-        o.x -= (o.px - (o.px = o.x)) * friction;
-        o.y -= (o.py - (o.py = o.y)) * friction;
+        for (i = 0; i < n; ++i) {
+          charges[i] = charge;
+        }
+        f = n * Math.abs(charge);
+      }
+      charge_abssum = f;
+    }
+    if (charge_abssum != 0) {
+      d3_layout_forceAccumulate(q, alpha, charges);
+      for (i = 0; i < n; ++i) {
+        if (!(o = nodes[i]).fixed) {
+          q.visit(repulse(o, i));
+        }
+      }
+    }
+    if (typeof repulsor === "function") {
+      repulsor.call(this, q, charges, distances, strengths);
+    }
+
+    // position verlet integration
+    if (typeof friction === "function") {
+      for (i = 0; i < n; ++i) {
+        o = nodes[i];
+        if (o.fixed) {
+          o.x = o.px;
+          o.y = o.py;
+        } else {
+          f = friction.call(this, o, i);
+          o.x -= (o.px - (o.px = o.x)) * f;
+          o.y -= (o.py - (o.py = o.y)) * f;
+        }
+      }
+    } else {
+      f = friction;
+      for (i = 0; i < n; ++i) {
+        o = nodes[i];
+        if (o.fixed) {
+          o.x = o.px;
+          o.y = o.py;
+        } else {
+          o.x -= (o.px - (o.px = o.x)) * f;
+          o.y -= (o.py - (o.py = o.y)) * f;
+        }
       }
     }
 
-    event.tick({type: "tick", alpha: alpha});
+    event.tick({type: "tick", alpha: alpha, quadtree: q});
   };
 
   force.nodes = function(x) {
@@ -138,6 +261,11 @@ d3.layout.force = function() {
     if (!arguments.length) return links;
     links = x;
     return force;
+  };
+
+  force.neighbors = function() {
+    neighbor(0);
+    return neighbors;
   };
 
   force.size = function(x) {
@@ -163,13 +291,14 @@ d3.layout.force = function() {
 
   force.friction = function(x) {
     if (!arguments.length) return friction;
-    friction = +x;
+    friction = typeof x === "function" ? x : +x;
     return force;
   };
 
   force.charge = function(x) {
     if (!arguments.length) return charge;
     charge = typeof x === "function" ? x : +x;
+    charge_abssum = -1;
     return force;
   };
 
@@ -181,13 +310,30 @@ d3.layout.force = function() {
 
   force.gravity = function(x) {
     if (!arguments.length) return gravity;
-    gravity = +x;
+    gravity = typeof x === "function" ? x : +x;
+    return force;
+  };
+
+  force.repulsor = function(x) {
+    if (!arguments.length) return repulsor;
+    repulsor = typeof x === "function" ? x : false;
     return force;
   };
 
   force.theta = function(x) {
-    if (!arguments.length) return Math.sqrt(theta2);
-    theta2 = x * x;
+    if (!arguments.length) {
+      if (has_theta2_f) {
+        return theta2;
+      } else {
+        return Math.sqrt(theta2);
+      }
+    }
+    // Set theta2 to x² when x is a value.
+    // (This is done as a calculation optimization for when rendering the force graph.)
+    //
+    // When x is a function, we need to do the squaring on every quad on every iteration anyhow.
+    theta2 = typeof x === "function" ? x : x * x;
+    setup_model_parameter_functors();
     return force;
   };
 
@@ -195,10 +341,10 @@ d3.layout.force = function() {
     if (!arguments.length) return alpha;
 
     x = +x;
-    if (alpha) { // if we're already running
-      if (x > 0) alpha = x; // we might keep it hot
-      else alpha = 0; // or, next tick will dispatch "end"
-    } else if (x > 0) { // otherwise, fire it up!
+    if (alpha) {                  // if we're already running
+      if (x > 0) alpha = x;       // we might keep it hot
+      else alpha = 0;             // or, next tick will dispatch "end"
+    } else if (x > 0) {           // otherwise, fire it up!
       event.start({type: "start", alpha: alpha = x});
       d3.timer(force.tick);
     }
@@ -207,16 +353,16 @@ d3.layout.force = function() {
   };
 
   force.start = function() {
-    var i,
+    var i, j,
         n = nodes.length,
         m = links.length,
         w = size[0],
         h = size[1],
-        neighbors,
         o;
 
     for (i = 0; i < n; ++i) {
-      (o = nodes[i]).index = i;
+      o = nodes[i];
+      o.index = i;
       o.weight = 0;
     }
 
@@ -230,49 +376,91 @@ d3.layout.force = function() {
 
     for (i = 0; i < n; ++i) {
       o = nodes[i];
-      if (isNaN(o.x)) o.x = position("x", w);
-      if (isNaN(o.y)) o.y = position("y", h);
+      if (isNaN(o.x)) o.x = position("x", w, i);
+      if (isNaN(o.y)) o.y = position("y", h, i);
       if (isNaN(o.px)) o.px = o.x;
       if (isNaN(o.py)) o.py = o.y;
     }
 
-    distances = [];
-    if (typeof linkDistance === "function") for (i = 0; i < m; ++i) distances[i] = +linkDistance.call(this, links[i], i);
-    else for (i = 0; i < m; ++i) distances[i] = linkDistance;
+    distances = new Array(m);
+    // for maximum performance, only use the function when there actually is the need for it:
+    if (typeof linkDistance === "function") {
+      for (i = 0; i < m; ++i) {
+        distances[i] = +linkDistance.call(this, links[i], i);
+      }
+    } else {
+      for (i = 0; i < m; ++i) {
+        distances[i] = linkDistance;
+      }
+    }
 
-    strengths = [];
-    if (typeof linkStrength === "function") for (i = 0; i < m; ++i) strengths[i] = +linkStrength.call(this, links[i], i);
-    else for (i = 0; i < m; ++i) strengths[i] = linkStrength;
+    strengths = new Array(m);
+    if (typeof linkStrength === "function") {
+      for (i = 0; i < m; ++i) {
+        strengths[i] = +linkStrength.call(this, links[i], i);
+      }
+    } else {
+      for (i = 0; i < m; ++i) {
+        strengths[i] = linkStrength;
+      }
+    }
 
-    charges = [];
-    if (typeof charge === "function") for (i = 0; i < n; ++i) charges[i] = +charge.call(this, nodes[i], i);
-    else for (i = 0; i < n; ++i) charges[i] = charge;
+    charges = new Array(n);
+    if (typeof charge === "function") {
+      j = 0;
+      for (i = 0; i < n; ++i) {
+        charges[i] = o = +charge.call(this, nodes[i], i);
+        j += Math.abs(o);
+      }
+    } else {
+      for (i = 0; i < n; ++i) {
+        charges[i] = charge;
+      }
+      j = n * Math.abs(charge);
+    }
+    charge_abssum = j;
 
     // inherit node position from first neighbor with defined position
     // or if no such neighbors, initialize node position randomly
     // initialize neighbors lazily to avoid overhead when not needed
-    function position(dimension, size) {
-      if (!neighbors) {
-        neighbors = new Array(n);
-        for (j = 0; j < n; ++j) {
-          neighbors[j] = [];
-        }
-        for (j = 0; j < m; ++j) {
-          var o = links[j];
-          neighbors[o.source.index].push(o.target);
-          neighbors[o.target.index].push(o.source);
-        }
-      }
-      var candidates = neighbors[i],
-          j = -1,
-          m = candidates.length,
+    function position(dimension, size, i) {
+      var candidates = neighbor(i),
+          j,
+          m = candidates.outlinks.length,
           x;
-      while (++j < m) if (!isNaN(x = candidates[j][dimension])) return x;
+      for (j = 0; j < m; ++j) {
+        if (!isNaN(x = candidates.outlinks[j].target[dimension]))
+          return x;
+      }
+      m = candidates.inlinks.length;
+      for (j = 0; j < m; ++j) {
+        if (!isNaN(x = candidates.inlinks[j].source[dimension]))
+          return x;
+      }
       return Math.random() * size;
     }
 
     return force.resume();
   };
+
+  // initialize neighbors lazily
+  function neighbor(i) {
+    if (!neighbors) {
+      var j,
+          n = nodes.length,
+          m = links.length;
+      neighbors = new Array(n);
+      for (j = 0; j < n; ++j) {
+        neighbors[j] = { inlinks: [], outlinks: [] };
+      }
+      for (j = 0; j < m; ++j) {
+        var o = links[j];
+        neighbors[o.source.index].outlinks.push(o);
+        neighbors[o.target.index].inlinks.push(o);
+      }
+    }
+    return neighbors[i];
+  }
 
   force.resume = function() {
     return force.alpha(.1);
@@ -331,6 +519,7 @@ function d3_layout_forceMouseout(d) {
 function d3_layout_forceAccumulate(quad, alpha, charges) {
   var cx = 0,
       cy = 0;
+
   quad.charge = 0;
   if (!quad.leaf) {
     var nodes = quad.nodes,
@@ -352,7 +541,7 @@ function d3_layout_forceAccumulate(quad, alpha, charges) {
       quad.point.x += Math.random() - .5;
       quad.point.y += Math.random() - .5;
     }
-    var k = alpha * charges[quad.point.index];
+    var k = charges[quad.point.index];
     quad.charge += quad.pointCharge = k;
     cx += k * quad.point.x;
     cy += k * quad.point.y;
@@ -364,3 +553,4 @@ function d3_layout_forceAccumulate(quad, alpha, charges) {
 var d3_layout_forceLinkDistance = 20,
     d3_layout_forceLinkStrength = 1,
     d3_layout_forceChargeDistance2 = Infinity;
+
