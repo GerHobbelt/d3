d5197dd6c8c5803d427bc1656ffc83655d1f1247
Ger Hobbelt <ger@hobbelt.com>
2014-08-28 01:01:23 +0200

    fix regression for environments which do not have the global 'window' object

--- src/event/mouse.js
+++ src/event/mouse.js
@@ -6,7 +6,7 @@ d3.mouse = function(container) {
 };
 
 // https://bugs.webkit.org/show_bug.cgi?id=44083
-var d3_mouse_bug44083 = /WebKit/.test(d3_window.navigator.userAgent) ? -1 : 0;
+var d3_mouse_bug44083 = (typeof d3_window !== 'undefined' && d3_window.navigator && /WebKit/.test(d3_window.navigator.userAgent)) ? -1 : 0;
 
 function d3_mousePoint(container, e) {
   if (e.changedTouches) e = e.changedTouches[0];
